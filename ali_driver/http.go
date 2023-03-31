04fdc3af2c4add10a1b04e275bcefc847d953bd6
YanJunhui <i@yanjunhui.com>
2023-03-31 13:31:23 +0800

    增加Llinux版本编译命令

--- ali_driver/http.go
+++ ali_driver/http.go
@@ -9,18 +9,6 @@ const (
 	APITVPanBase = "https://api.tv-pan.com"
 	APITVPanQR   = APITVPanBase + "/ali/qrcode"
 	APITVPanAuth = APITVPanBase + "/ali/auth"
-
-	APIBase              = "https://open.aliyundrive.com"
-	APIList              = APIBase + "/adrive/v1.0/openFile/list"
-	APIFile              = APIBase + "/adrive/v1.0/openFile/get"
-	APIDriveInfo         = APIBase + "/adrive/v1.0/user/getDriveInfo"
-	APIRemoveTrash       = APIBase + "/adrive/v1.0/openFile/recyclebin/trash"        //移动到垃圾箱
-	APICreateFolder      = APIBase + "/adrive/v1.0/openFile/create"                  //创建目录
-	APIFileDownload      = APIBase + "/adrive/v1.0/openFile/getDownloadUrl"          //获取下载链接
-	APIFileVideoPlayInfo = APIBase + "/adrive/v1.0/openFile/getVideoPreviewPlayInfo" //获取视频转码播放信息
-	APIFileMove          = APIBase + "/adrive/v1.0/openFile/move"                    //移动文件
-	APIFileUpdate        = APIBase + "/adrive/v1.0/openFile/update"                  //更新文件
-	APISpaceInfo         = APIBase + "/adrive/v1.0/user/getSpaceInfo"                //获取空间信息
 )
 
 var RestyHttpClient = NewRestyClient()
