96511a863fed97303698bb8bb501a52ea8a6bb7a
闫军辉 <i@yanjunhui.com>
2023-03-31 13:45:05 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o webdav_Mac_amd64
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o webdav_Mac_arm64
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o webdav_Win_amd64.exe
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o webdav_Linux_amd64.exe
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o webdav_Linux_amd64
